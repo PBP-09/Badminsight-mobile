33440221de6a14298e7007a6425a6fb7c4230a81
RousanChandra <syahrousan@gmail.com>
2025-12-22 00:12:22 +0700

    ayam

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -103,6 +103,7 @@ class _LoginPageState extends State<LoginPage> {
                       // TODO: Change the URL and don't forget to add trailing slash (/) at the end of URL!
                       // To connect Android emulator with Django on localhost, use URL http://10.0.2.2/
                       // If you using chrome,  use URL http://localhost:8000
+                      
                       final response = await request
                           .login("http://rousan-chandra-badminsights.pbp.cs.ui.ac.id/auth/login/", {
                         'username': username,
